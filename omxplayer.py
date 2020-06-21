9ab1e2220daabcb7b64a4e88049af3c235f544b1
Sachin Ramanathan <sachin.ramanathan123@gmail.com>
2020-06-21 11:47:45 +0800

    Full path

--- omxplayer.py
+++ omxplayer.py
@@ -7,10 +7,10 @@ class OMX():
             # ydl = YoutubeDL()
             # r = ydl.extract_info(file, download=False)
             # url = [f['url'] for f in r['formats'] if f['acodec'] != 'none' and f['vcodec'] != 'none'][-1]
-            file = f"$(youtube-dl -g -f best '{file}')"
-        self.p = Popen("/usr/bin/omxplayer --no-osd" + " " + file,
+            file = f"$(/home/pi/.local/bin/youtube-dl -g -f best '{file}')"
+        self.p = Popen(["/usr/bin/omxplayer", "--no-osd", file],
             stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True,
-                bufsize=0, close_fds=True, shell=True)
+                bufsize=0, close_fds=True)
 
     def playpause(self):
         self.p.stdin.write("p")
