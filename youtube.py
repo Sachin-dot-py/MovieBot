114629c95cb82cd63550e82cba2d06026f251440
Sachin Ramanathan <sachin.ramanathan123@gmail.com>
2020-06-23 12:12:47 +0800

    Fixed error with quotation marks

--- youtube.py
+++ youtube.py
@@ -6,7 +6,7 @@ def download(url):
     file_command = f"youtube-dl -q --get-filename -f mp4 -o '~/Movies/%(title)s.%(ext)s' '{url}'"
     process = subprocess.Popen(file_command, shell=True, stdout=subprocess.PIPE)
     file = process.stdout.readline().rstrip().decode('UTF-8')
-    name = f'"{file.split('Movies/')[-1].strip('.mp4')}"'
+    name = f'"{file.split("Movies/")[-1].strip(".mp4")}"'
     return name
 
 if __name__ == "__main__":
