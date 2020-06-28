3f3df91e893a51cca651f0bd2197fa9b59a64e3c
Sachin Ramanathan <sachin.ramanathan123@gmail.com>
2020-06-28 09:21:44 +0800

    Removed YouTube functionality

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from credentials import TELEGRAM_TOKEN, CHAT_ID
 from einthusan import downloadMovie, einthusanDetails
 from omxplayer import OMX
-from youtube import downloadVideo
 import logging
 import os
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
@@ -95,8 +94,8 @@ def download(update, context):
         return
     if 'youtu' in query:
         context.bot.send_message(chat_id=chat_id,
-                                 text=f"Downloading {query}...")
-        movie_name = downloadVideo(query)
+                                 text=f"Playing your youtube video...")
+        # TODO Play the video and give controls
     else:
         movie_name, einthusan_link = einthusanDetails(query)
         context.bot.send_message(chat_id=chat_id,
